5e6cd67098f9a4f6b6f7a97a8170f1728e91004d
eunbeom.kang <eunbeom.kang@linecorp.com>
2021-03-10 22:34:46 +0900

    날씨

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'dart:async';
 
 import 'package:flutter/material.dart';
+import 'package:weather/weather.dart';
 
 void main() {
   runApp(MyApp());
@@ -24,6 +25,7 @@ class MyHomePage extends StatefulWidget {
 class TimeZone {
   String name;
   Duration timeZoneOffset;
+  Weather weather;
 
   TimeZone(this.name, int offset) {
     this.timeZoneOffset = Duration(hours: offset);
@@ -36,19 +38,20 @@ class _MyHomePageState extends State<MyHomePage> {
     TimeZone('Hong Kong', 8),
     TimeZone('Berlin', 1),
     TimeZone('London', 0),
-    TimeZone('New York / Toronto', -5),
-    TimeZone('Los Angeles / Seattle', -8),
+    TimeZone('New York', -5),
+    TimeZone('Toronto', -5),
+    TimeZone('Los Angeles', -8),
+    TimeZone('Seattle', -8),
   ];
-
-  DateTime utc = DateTime.now().toUtc();
 
   @override
   void initState() {
     super.initState();
-    init();
+    loop();
+    weather();
   }
 
-  void init() async {
+  void loop() async {
     while (true) {
       update();
       await Future.delayed(Duration(seconds: 1));
@@ -56,11 +59,19 @@ class _MyHomePageState extends State<MyHomePage> {
   }
 
   update() async {
-    utc = DateTime.now().toUtc();
     setState(() {});
   }
 
+  void weather() async {
+    WeatherFactory wf = WeatherFactory('34447a7ec0ffb652b17e901ef4e88004');
+    for (var i = 0; i < data.length; i++) {
+      data[i].weather = await wf.currentWeatherByCityName(data[i].name);
+      setState(() {});
+    }
+  }
+
   String getTimeText(int index) {
+    var utc = DateTime.now().toUtc();
     var time = utc.add(data[index].timeZoneOffset);
     var hour = time.hour.toString().padLeft(2, '0');
     var minute = time.minute.toString().padLeft(2, '0');
@@ -71,25 +82,52 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: ListView.builder(
-        itemCount: data.length,
-        itemBuilder: (context, index) {
-          return Padding(
-            padding: EdgeInsets.all(10),
-            child: Column(
-              children: [
-                Text(
-                  data[index].name,
-                  style: TextStyle(fontSize: 20),
-                ),
-                Text(
-                  getTimeText(index),
-                  style: TextStyle(fontSize: 40, fontWeight: FontWeight.bold),
-                )
-              ],
+      backgroundColor: Color.fromARGB(255, 242, 239, 228),
+      body: Center(
+        child: Container(
+          width: 500,
+          child: GridView.count(
+            childAspectRatio: 1.3,
+            crossAxisCount: 2,
+            children: List.generate(
+              data.length,
+              (index) {
+                return Center(
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Text(
+                            data[index].name,
+                            style: TextStyle(fontSize: 20),
+                          ),
+                          data[index].weather != null
+                              ? Image.network(
+                                  'http://openweathermap.org/img/wn/${data[index].weather.weatherIcon}@2x.png',
+                                  width: 50,
+                                  height: 50,
+                                )
+                              : Container(),
+                          data[index].weather != null
+                              ? Text(
+                                  '${data[index].weather.temperature.celsius.ceil()}°C')
+                              : Container()
+                        ],
+                      ),
+                      Text(
+                        getTimeText(index),
+                        style: TextStyle(
+                            fontSize: 40, fontWeight: FontWeight.bold),
+                      )
+                    ],
+                  ),
+                );
+              },
             ),
-          );
-        },
+          ),
+        ),
       ),
     );
   }
