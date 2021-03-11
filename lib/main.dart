2684bcc0c5baed8aa45a2979e8c63a2afc7343cc
eunbeom.kang <eunbeom.kang@linecorp.com>
2021-03-12 00:46:46 +0900

    카드 배경색 제거

--- lib/main.dart
+++ lib/main.dart
@@ -115,14 +115,11 @@ class _MyHomePageState extends State<MyHomePage> {
               final item = data[index];
               return DecoratedBox(
                 decoration: BoxDecoration(
-                    image: DecorationImage(
-                      image: AssetImage(
-                          'assets/city_${item.displayName.replaceAll(' ', '_').toLowerCase()}.png'),
-                    ),
-                    color: Colors.black,
-                    borderRadius: BorderRadius.all(
-                      Radius.circular(16),
-                    )),
+                  image: DecorationImage(
+                    image: AssetImage(
+                        'assets/city_${item.displayName.replaceAll(' ', '_').toLowerCase()}.png'),
+                  ),
+                ),
                 child: Stack(
                   children: [
                     item.weather != null
